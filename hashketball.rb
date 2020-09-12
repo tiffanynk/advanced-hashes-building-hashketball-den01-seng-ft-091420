64f02f293c7ba1c3ea562623da5e212ed53982cf
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 18:13:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,17 @@ def game_hash
       team_name:  "",
       colors: [""],
       players: [
-        {}
+        {
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "",
+          blocks: "",
+          slam_dunks: "",
+        }
       ]
     },
     away: {}
