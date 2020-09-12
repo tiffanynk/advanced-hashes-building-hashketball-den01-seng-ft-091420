dd0362f710994fb0274b97147c3f0ee785e8a9e3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 18:20:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,7 +49,7 @@ def game_hash
           slam_dunks: 5,
         },
         {
-          player_name: "Jerry Terry",
+          player_name: "Jason Terry",
           number: 31,
           shoe: 15,
           points: 19,
