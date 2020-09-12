f08e93f5bad0d4bc7ba98d76abcb050e0d122020
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 18:17:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,7 +25,18 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7,          
-        }
+        },
+        {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15,          
+        },
       ]
     },
     away: {}
