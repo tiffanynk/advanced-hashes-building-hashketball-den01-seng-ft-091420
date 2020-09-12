619ae29651d8f3fa419aa579cd254003a9ec1d57
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 18:19:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -35,7 +35,7 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunks: 15,          
+          slam_dunks: 15,
         },
         {
           player_name: "Mason Plumlee",
