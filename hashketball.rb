df50ee5f7b590580fd58757b3223c69ea9276fe9
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 18:15:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,13 +7,13 @@ def game_hash
         {
           player_name: "Alan Anderson",
           number: 0,
-          shoe: "",
-          points: "",
-          rebounds: "",
-          assists: "",
-          steals: "",
-          blocks: "",
-          slam_dunks: "",
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1,
         }
       ]
     },
